cbb091e4a74ddb5bbd8f27ba747ef686c7441ca5
Ubuntu <ubuntu@ip-172-31-30-102.ec2.internal>
2021-05-03 20:14:55 +0000

    Atualizacoes de versao

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
@@ -10,7 +10,10 @@ public class Receptor {
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setUri("amqp://...");
+    factory.setHost("ip-da-instancia-da-aws"); // Alterar
+    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
+    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setVirtualHost("/");    Connection connection = factory.newConnection();
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
 
